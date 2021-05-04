e4f2a48572c44fdb23dfd983ad66014ada0a9d50
sebastian <jsbenavides69@ucatolica.edu.co>
2021-05-04 16:17:48 -0500

    Navbar menu

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Logo from '../../images/Logo.png'
+//FontAwesome 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from "@fortawesome/free-solid-svg-icons"
 
 const Navbar = () => {
     return (
@@ -10,7 +13,7 @@ const Navbar = () => {
                 <img className="logo" src={Logo} alt="Logo" />
             </a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
+                <FontAwesomeIcon icon={ faBars } style= {{color: "#fff"}}/>
             </button>
         
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
