f5b6cbebca1bdecd9c3e02b69d757e72c54ba0be
sebastian <jsbenavides69@ucatolica.edu.co>
2021-05-10 16:25:30 -0500

    header ready

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
 import React from 'react';
 import './App.css';
-import Navbar from './Components/Navbar/index'
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import About from './Components/pages/About'
-import Project from './Components/pages/projects'
-import Contact from './Components/pages/Contact'
+import Particles from 'react-particles-js';
+import Navbar from './Components/index'
+import About from './Components/About'
+import Project from './Components/projects'
+import Contact from './Components/Contact'
 
 function App() {
   return (
-    <Router>
+    <>
+      <Particles 
+        className="particles-canvas"
+        params= {{
+                  particles: {
+                    number:{
+                      value: 60,
+                      density:{
+                        enabled: true,
+                        value_area: 900
+                      }
+                    },
+                    shape: {
+                      type: 'circle',
+                      stroke:{
+                        width: 8,
+                        color: "#f9ab00"
+                      }
+                    }
+                  }
+        }}
+      />
       <Navbar />
       <About />
-      <Project />
-      <Contact />
-    </Router>
+      {/* <Project />
+      <Contact /> */}
+      </>
   );
 };
 
